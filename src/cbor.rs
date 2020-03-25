b12664bdfb6f027801f878cf682094336b84cf6c
Eric Seppanen <eds@reric.net>
2020-03-25 11:23:49 -0700

    more work
    Things are looking pretty good.  The new form of Validate is working
    well, though I need it to be able to return something other than ()
    for Map lookup to work properly.

--- src/cbor.rs
+++ src/cbor.rs
@@ -1,29 +1,110 @@
-use serde_cbor::{self, Value};
 use crate::ivt::*;
+use serde_cbor::{self, Value};
+use std::cell::RefCell;
+use std::collections::BTreeMap; // used in serde_cbor Value::Map
+
+type ValueMap = BTreeMap<Value, Value>;
+
+/// This struct allows us to maintain a map that is consumed during validation.
+struct WorkingMap {
+    map: RefCell<ValueMap>,
+}
+
+impl WorkingMap {
+    /// Makes a copy of an existing map's table.
+    fn new(value_map: &ValueMap) -> WorkingMap {
+        WorkingMap {
+            map: RefCell::new(value_map.clone()),
+        }
+    }
+}
 
 fn make_oops(msg: &str) -> ValidateResult {
     Err(ValidateError::Oops(msg.into()))
 }
 
-impl Validate<Value> for Node {
-    fn validate(&self, value: &Value) -> ValidateResult {
-        match self {
+impl Validate for Value {
+    // This is the main validation dispatch function.
+    // It tries to match a Node and a Value, recursing as needed.
+    fn validate(&self, node: &Node) -> ValidateResult {
+        let value = self;
+        match node {
+            Node::Literal(_) => unimplemented!(),
             Node::PreludeType(p) => validate_prelude_type(p, value),
             Node::Choice(c) => validate_choice(c, value),
-            _ => unimplemented!()
+            Node::Map(m) => validate_map(m, value),
+            Node::ArrayRecord(_) => unimplemented!(),
+            Node::ArrayVec(_) => unimplemented!(),
+            Node::Rule(_) => unreachable!(), // Should never happen
         }
     }
 }
 
-// This is a strange hack, to allow generic validation functions to live
-// in the "ivt" module while still calling back here.
-// I really wish I could find a way around this.
-impl Pogo<Node> for Value {
-    fn pogo(&self, node: &Node) -> ValidateResult {
-        node.validate(self)
+impl Validate for WorkingMap {
+    // Dispatch for handling Map key types.  This is specialized because
+    // some keys (literal values) can be found with a fast search, while
+    // others may require a linear search.
+    fn validate(&self, node: &Node) -> ValidateResult {
+        let value_map = self;
+        match node {
+            Node::Literal(l) => map_search_literal(l, value_map),
+            _ => map_search(node, value_map),
+        }
     }
 }
 
+impl From<&Literal> for Value {
+    fn from(l: &Literal) -> Value {
+        match l {
+            Literal::Bool(b) => Value::Bool(*b),
+            Literal::Int(i) => Value::Integer(*i),
+            Literal::Float(f) => Value::Float(*f),
+        }
+    }
+}
+
+fn map_search_literal(literal: &Literal, working_map: &WorkingMap) -> ValidateResult {
+    // Find a key in the working map, looking for a match.
+    // If we find one, remove that key.
+    let mut working_mut = working_map.map.borrow_mut();
+    let search_key = Value::from(literal);
+    match working_mut.remove(&search_key) {
+        Some(_) => {
+            // We found the key, and removed it from the working map.
+            // This means validation was successful.
+            Ok(())
+        },
+        None => {
+            // We didn't find the key; return an error
+            make_oops("failed map_search_literal")
+        }
+    }
+}
+
+fn map_search(node: &Node, working_map: &WorkingMap) -> ValidateResult {
+    // Iterate over each key in the working map, looking for a match.
+    // If we find one, remove that key.
+    // This is less efficient than map_search_literal.
+    let mut working_mut = working_map.map.borrow_mut();
+    for key in working_mut.keys() {
+        let attempt = key.validate(node);
+        match attempt {
+            Ok(_) => {
+                // This key matches the node.  Remove the key and return success.
+                // Some juggling is required to satisfy the borrow checker.
+                let key2 = key.clone();
+                drop(key);
+                working_mut.remove(&key2);
+                return Ok(());
+            }
+            Err(_) => {
+                // This key didn't match, but maybe another one will
+            }
+        }
+    }
+    // We searched all the keys without finding a match.  Validation fails.
+    make_oops("failed map_search")
+}
 
 fn validate_prelude_type(ty: &PreludeType, value: &Value) -> ValidateResult {
     match (ty, value) {
@@ -33,7 +114,43 @@ fn validate_prelude_type(ty: &PreludeType, value: &Value) -> ValidateResult {
     }
 }
 
+fn validate_map(m: &Map, value: &Value) -> ValidateResult {
+    match value {
+        Value::Map(vm) => validate_map_part2(m, vm),
+        _ => make_oops("expected map, found not-a-map"),
+    }
+}
 
+fn exact_key_search(target: &Literal, value_map: &ValueMap) -> bool {
+    false
+}
+
+fn validate_map_part2(m: &Map, value_map: &ValueMap) -> ValidateResult {
+    // Strategy for validating a map:
+    // 1. We assume that the code that constructed the IVT Map placed the keys
+    //    in matching order (specific heads first, more general types at the end)
+    //    so that we consume IVT Map keys in order without worrying about non-
+    //    deterministic results.
+    // 2. Make a mutable working copy of the Value::Map contents
+    // 3. Iterate over the IVT Map, searching the Value::Map for a matching key.
+    // 4. If a matching key is found, validate the key's corresponding value.
+    // 5. If a match is found, remove the key-value pair from our working copy.
+    // 6. If the key can consume multiple values, repeat the search for this key.
+    // 7. If the key is not found and the key is optional, continue to the next key.
+    // 8. If the key is not found and the key is not optional, return an error.
+
+    let mut working_map = WorkingMap::new(value_map);
+
+    for kv in &m.members {
+        let key = kv.key.as_ref();
+        working_map.validate(key)?;
+        //let value = kv.value;
+        // Validate the key and then the value
+
+        //kv.key, kv.value
+    }
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
--- src/ivt.rs
+++ src/ivt.rs
@@ -24,65 +24,76 @@ impl error::Error for ValidateError {
 
 pub type ValidateResult = std::result::Result<(), ValidateError>;
 
-pub trait Validate<T> {
-    fn validate(&self, value: &T) -> ValidateResult;
+pub trait Validate {
+    fn validate(&self, node: &Node) -> ValidateResult;
 }
-
-pub trait Pogo<N> {
-    fn pogo(&self, node: &N) -> ValidateResult;
-}
-
-//type DynVal<T> = Box<dyn Validate<T>>;
 
 // Some useful type shortcuts
 pub type ArcNode = Arc<Node>;
 pub type VecNode = Vec<ArcNode>;
 
 // One of the types named in the CDDL prelude
+#[derive(Debug, Copy, Clone)]
 pub enum PreludeType {
     Int,
     Tstr,
     Bstr,
 }
 
+// A literal value, e.g. 7, 1.3, or "foo"
+#[derive(Debug, Copy, Clone)]
+pub enum Literal {
+    Bool(bool),
+    Int(i128),
+    Float(f64),
+    // TODO: byte string literals, nil?
+}
+
 // A rule reference, by name.
 // FIXME: how can I replace these with actual owning references (e.g. Arc<Node>)?
 // I think what happens is that the Rule node just gets replaced with an ArcNode that
 // shares ownership of the RHS of the rule (which can be a type, array, map, etc.)
+#[derive(Debug, Clone)]
 pub struct Rule {
     pub name: String,
 }
 
 // Something that validates if it matches any of the options.
+#[derive(Debug, Clone)]
 pub struct Choice {
     pub options: VecNode,
 }
 
 // Intermediate Key-value pair; key and value can be anything (types, arrays, maps, etc.)
+#[derive(Debug, Clone)]
 pub struct KeyValue {
     pub key: ArcNode,
     pub value: ArcNode,
 }
 
+#[derive(Debug, Clone)]
 pub struct Map {
     pub members: Vec<KeyValue>,
 }
 
+#[derive(Debug, Clone)]
 pub struct ArrayRecord {
     pub elements: Vec<KeyValue>,
 }
 
+#[derive(Debug, Clone)]
 pub struct ArrayVec {
     // TODO: handle occurrences
     pub element: ArcNode,
 }
 
 // Any node in the Intermediate Validation Tree
+#[derive(Debug, Clone)]
 pub enum Node {
+    Literal(Literal),
     PreludeType(PreludeType),
     Rule(Rule),
     Choice(Choice),
-    KeyValue(KeyValue),
     Map(Map),
     ArrayRecord(ArrayRecord),
     ArrayVec(ArrayVec),
@@ -95,34 +106,19 @@ impl From<PreludeType> for Node {
     }
 }
 
-pub fn validate_choice<T>(choice: &Choice, value: &T) -> ValidateResult
-where
-    T: Pogo<Node>,
-{
+// This is just a convenience function, that reverses Node and Value, because
+// it's more intuitive to write node.validate(value) than value.validate(node).
+impl Node {
+    fn validatex<T: Validate>(&self, value: &T) -> ValidateResult {
+        value.validate(self)
+    }
+}
+
+pub fn validate_choice<T: Validate>(choice: &Choice, value: &T) -> ValidateResult {
     // A choice validates if any of the options validates
     for node in &choice.options {
         let node: &Node = node.as_ref();
-        // This generates a compiler error "expected ___, found type parameter `T`"
-        // It would be a lot more graceful if this had worked.
-        // Instead, we have this Pogo workaround.
-        //node.validate(value)?;
-        value.pogo(node)?;
+        value.validate(node)?;
     }
     Ok(())
 }
-
-// FIXME: I can't figure out how to implement this logic.
-// I can kind of see how it's impossible to dispatch the inner call
-// to validate() because we can't know at compile-time what function
-// to call, or even if one exists.
-/*
-impl<T> Validate<T> for Choice {
-    fn validate(&self, value: &T) -> ValidateResult {
-        // A choice validates if any of the options validates
-        for node in self.options {
-            node.validate::<T>(value)?;
-        }
-        Ok(())
-    }
-}
-*/
